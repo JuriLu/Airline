ac6c7bb47031bb06d2c989941c6f89b6da814358
JurgenLubonja <jurgen.lubonja@eng.it>
2023-10-25 11:56:31 +0200

    Json db

--- src/app/passenger-dashboard/services/passenger-dashboard.service.ts
+++ src/app/passenger-dashboard/services/passenger-dashboard.service.ts
@@ -11,55 +11,6 @@ export class PassengerDashboardService {
 
   getPassengers():Passenger[] {
   
-    return [
-      {
-        id:1,
-        fullname: 'Stephen Curry',
-        checkedIn: true,
-        checkInDate: 1687501476000,
-        children:[
-          {
-            name:'Jasmine Curry',
-            age:4
-        }]
-      },
-      {
-        id:2,
-        fullname: 'Rose Chillingham',
-        checkedIn: false,
-        checkInDate: 11697490148000  
-      },
-      {
-        id:3,
-        fullname: 'James Brown',
-        checkedIn: true,
-        checkInDate: 1702581969000,
-        children:[
-          {
-            name:'David Brown',
-            age:5
-        }] 
-    
-      },
-      {
-        id:4,
-        fullname: 'Louise Sumon',
-        checkedIn: true,
-        checkInDate: 1684349763000 
-    
-      },
-      {
-        id:5,
-        fullname: 'Tina Craig',
-        checkedIn: false,
-        checkInDate: 1687857663000,
-        children:[
-          {
-            name:'Julia Craig',
-            age:6
-        }] 
-    
-      }
-      ]
+    return 
   }
 }
