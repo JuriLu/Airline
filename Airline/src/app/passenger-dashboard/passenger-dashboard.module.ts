39dcbd013a19e0538bf833a61cd324a03f4490c4
jlubonja <lubonjajurgen1@gmail.com>
2024-11-14 23:29:26 +0100

    Upgrade to ng 18

--- Airline/src/app/passenger-dashboard/passenger-dashboard.module.ts
+++ Airline/src/app/passenger-dashboard/passenger-dashboard.module.ts
@@ -5,21 +5,15 @@ import { RouterModule } from '@angular/router';
 import { ROUTES } from './routes';
 import { PassengerDetailComponent } from './components/passenger-detail/passenger-detail.component';
 import { PassengerCountComponent } from './components/passenger-count/passenger-count.component';
-import {HttpClientModule} from "@angular/common/http";
+import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 
 
 
 
-@NgModule({
-  declarations: [
-    PassengerDashboardComponent,
-    PassengerDetailComponent,
-    PassengerCountComponent
-  ],
-  imports: [
-    CommonModule,
-    RouterModule.forChild(ROUTES),
-    HttpClientModule
-  ]
-})
+@NgModule({ declarations: [
+        PassengerDashboardComponent,
+        PassengerDetailComponent,
+        PassengerCountComponent
+    ], imports: [CommonModule,
+        RouterModule.forChild(ROUTES)], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class PassengerDashboardModule { }
